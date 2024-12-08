ca19154f89ad3ae3920494b03242d1a18ec7b1f7
JeanpeterP <jeanpeterpolanco@gmail.com>
2024-12-08 01:20:27 -0400

    customization imrpoved

--- frontend/src/components/watermarks/getWatermarkSVG.ts
+++ frontend/src/components/watermarks/getWatermarkSVG.ts
@@ -1,19 +1,23 @@
 import SantaClausMask from './SantaClausWatermark';
-
+import SnowflakeMask from './SnowflakeWatermark';
+import SkullMask from './SkullWatermark';
+import CandyCaneMask from './CandyCaneWatermark';
+import ReindeerMask from './ReindeerWatermark';
+import ChristmasTreeMask from './ChristmasTreeWatermark';
 export const getWatermarkSVG = (watermark: string, color: string): string => {
   switch (watermark) {
-    // case 'skull':
-    //   return SkullMask.replace('fill="black"', `fill="${color}"`);
-    // case 'candycane':
-    //   return CandyCaneMask.replace('fill="black"', `fill="${color}"`);
-    // case 'snowflake':
-    //   return SnowflakeMask.replace('fill="black"', `fill="${color}"`);
+    case 'skull':
+      return SkullMask.replace(/fill="currentColor"/g, `fill="${color}"`);
+    case 'candycane':
+      return CandyCaneMask.replace(/fill="currentColor"/g, `fill="${color}"`);
+    case 'snowflake':
+      return SnowflakeMask.replace(/fill="currentColor"/g, `fill="${color}"`);
     case 'santaclaus':
       return SantaClausMask.replace(/fill="currentColor"/g, `fill="${color}"`);
-    // case 'reindeer':
-    //   return ReindeerMask.replace('fill="black"', `fill="${color}"`);
-    // case 'christmastree':
-    //   return ChristmasTreeMask.replace('fill="black"', `fill="${color}"`);
+    case 'reindeer':
+      return ReindeerMask.replace(/fill="currentColor"/g, `fill="${color}"`);
+    case 'christmastree':
+      return ChristmasTreeMask.replace(/fill="currentColor"/g, `fill="${color}"`);
     default:
       return '';
   }
